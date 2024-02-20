43b7803300c359c169c8a215abdc323b791ca804
Ivana Djordjevic <127266659+runningaroundintheabyss@users.noreply.github.com>
2024-02-19 17:52:01 -0800

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,22 @@
 //* A single `Footer` component that appears on multiple pages
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+
 export default function Header() {
     return (
-        <footer className="justify-content-center border-style">
-            <h6>Hi i am the footer </h6> 
+        <footer className="border-style">
+            <a href='https://github.com/Ivana-Djordjevic'>
+                <FontAwesomeIcon className="icon" icon={faGithub} />
+            </a>
+            <a href='https://www.linkedin.com/in/ivana-djordjevic-799351268/'>
+                <FontAwesomeIcon className="icon" icon={faLinkedin} />
+            </a>
+            <a href='https://www.instagram.com/runningaroundintheabyss?igsh=YzVkODRmOTdmMw%3D%3D&utm_source=qr'>
+                <FontAwesomeIcon className="icon" icon={faInstagram} />
+            </a>
         </footer>
 
-    )
+    );
 }
